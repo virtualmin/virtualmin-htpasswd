f897efafb196a512d74305a15da58078b7599317
Jamie Cameron <jcameron@webmin.com>
2009-03-01 23:22:18 +0000

    Use new module

--- virtualmin-htpasswd-lib.pl
+++ virtualmin-htpasswd-lib.pl
@@ -1,8 +1,12 @@
 # Common functions for simple protected directory management
 
-do '../web-lib.pl';
+BEGIN { push(@INC, ".."); };
+eval "use WebminCore;";
+if ($@) {
+	do '../web-lib.pl';
+	do '../ui-lib.pl';
+	}
 &init_config();
-do '../ui-lib.pl';
 &foreign_require("htaccess-htpasswd", "htaccess-lib.pl");
 &foreign_require("virtual-server", "virtual-server-lib.pl");
 &foreign_require("apache", "apache-lib.pl");
