04f3ca51c2391ce9621c7d249e7e597fe3cae58d
Jamie Cameron <jcameron@webmin.com>
2009-06-12 21:48:37 +0000

    Participate in Webmin search

---
+++ cgi_args.pl
@@ -0,0 +1,13 @@
+
+do 'virtualmin-htpasswd-lib.pl';
+
+sub cgi_args
+{
+my ($cgi) = @_;
+my ($d) = grep { &virtual_server::can_edit_domain($_) &&
+	         $_->{$module_name} } &virtual_server::list_domains();
+if ($cgi eq 'add_form.cgi') {
+	return $d ? 'dom='.$d->{'id'} : 'none';
+	}
+return undef;
+}
